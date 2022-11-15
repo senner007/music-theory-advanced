3278233bad915df189577e4bd5a979dce41a2a5e
rennes007 <nielshtg@htomail.com>
2022-11-15 07:49:54 +0100

    remove try catch

--- src/quizEngine/textQuiz.ts
+++ src/quizEngine/textQuiz.ts
@@ -2,15 +2,11 @@ import { IQuiz } from "../quiz-types";
 import { LogAsync } from "../utils/logAsync";
 
 export async function textQuiz(quiz: IQuiz): Promise<string | never> {
-    try {
-        const choice = await LogAsync.questionInListIndexed(
-            quiz.questionOptions,
-            quiz.question,
-            "q"
-        );
-        return choice;
 
-    } catch (err) {
-        throw (err);
-    }
+    const choice = await LogAsync.questionInListIndexed(
+        quiz.questionOptions,
+        quiz.question,
+        "q"
+    );
+    return choice;
 }
