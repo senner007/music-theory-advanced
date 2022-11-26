8bc2eedde6cae4246940f4dddd69ca1d2ff66bb4
Senner007 <nielshtg@hotmail.com>
2022-11-26 14:23:47 +0100

    misc refactor

--- src/logger/logTable.ts
+++ src/logger/logTable.ts
@@ -2,48 +2,49 @@
 import AsciiTable from "ascii-table";
 import { SolfegeMelody, Syllable } from "../solfege";
 
-  
 interface ITableObject {
   [key: string]: Syllable[];
 }
-
 
 function createTableObject(solfege: SolfegeMelody) {
   const ambitus = solfege.ambitus();
 
   const obj: ITableObject = {};
   for (let i = 0; i < ambitus + 1; i++) {
-    obj[i] = Array.from({ length: solfege.duration });
+    obj[i] = [...Array(solfege.duration)]
   }
   return obj;
 }
 
-function fillTableObject(solfege: SolfegeMelody, tableObject: ITableObject) {
+function fillRows(solfege: SolfegeMelody, tableObject: ITableObject) {
   let totalDuration: number = 0;
 
   solfege.getMelody.forEach((melodyNote) => {
-   
-    melodyNote.noteNames.forEach(n => {
-      const pitchRow: number = solfege.distance_from_lowest(n)
+    melodyNote.noteNames.forEach((n) => {
+      const pitchRow: number = solfege.distance_from_lowest(n);
       const pitchSyllable = solfege.syllable(n);
       tableObject[pitchRow][totalDuration] = pitchSyllable;
-    })
-   
+    });
+
     totalDuration = totalDuration + melodyNote.duration;
   });
 
   return Object.values(tableObject).reverse();
 }
 
+function headingInMeasures(solfege: SolfegeMelody) {
+  return [...Array(solfege.duration).keys()]
+    .map((h) => (h % 4 === 0 ? "0" + (h / 4 + 1) : "  "))
+    .map((h, index) => (index === 0 ? "01" : h));
+}
+
 export class LogTable {
   static write(solfege: SolfegeMelody) {
     const tableObject = createTableObject(solfege);
-    const rows = fillTableObject(solfege, tableObject);
+    const rows = fillRows(solfege, tableObject);
 
     var table = AsciiTable.factory({
-      heading: [...Array(solfege.duration).keys()]
-        .map(h => h % 4 === 0 ? "0" + (h/4 + 1) : "  ")
-        .map((h, index) => index === 0 ? "01" : h),
+      heading: headingInMeasures(solfege),
       rows: rows,
     });
 
