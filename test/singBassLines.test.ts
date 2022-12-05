85e18cc66a657303196a2ff91e9f601f728f4247
Senner007 <nielshtg@hotmail.com>
2022-12-05 14:22:18 +0100

    sing bass lines test

---
+++ test/singBassLines.test.ts
@@ -0,0 +1,98 @@
+import { expect, vi, describe, test, afterEach, Mock } from "vitest";
+import chalk from "chalk";
+import { MathFloor } from "../src/random-funcs";
+import { SingBassLines } from "../src/quiz/singBassLines";
+import { LogTable } from "../src/logger/logTable";
+import { LogAsync } from "../src/logger/logAsync";
+LogAsync;
+
+describe("Test SingBassLines quiz", () => { // put in mocks folder
+  // Mock the MathFloor method to remove randomness
+  vi.mock("../src/random-funcs", () => {
+    return {
+      MathFloor: vi.fn(),
+    };
+  });
+
+  vi.mock("../src/logger/logTable", () => {
+    const LogTableMock = vi.fn();
+    (LogTableMock as any).write = vi.fn();
+
+    return { LogTable: LogTableMock };
+  });
+
+  vi.mock("../src/midiplay", () => {
+    return {
+      playMidi: vi.fn(),
+    };
+  });
+
+  vi.mock("../src/logger/logAsync", () => {
+    const LogAsyncMock = vi.fn();
+    (LogAsyncMock as any).questionInListIndexedGlobalKeyHook = vi.fn();
+
+    return { LogAsync: LogAsyncMock };
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  const quizHeadOutput = [
+    `${chalk.underline("Diatonic")} progression bass line in key of ${chalk.underline("Cb Major")}`,
+    `${chalk.underline("Diatonic")} progression bass line in key of ${chalk.underline("D Minor")}`,
+    `${chalk.underline("Diatonic")} progression bass line in key of ${chalk.underline("E# Minor")}`,
+  ];
+
+  const solfegeMelodies = [
+    {
+      sortedMelody: [ 'Fb3', 'Gb3', 'Cb4', 'Cb4' ],
+      melody: [
+        { noteNames: ['Cb4'], duration: 1 },
+        { noteNames: ['Fb3'], duration: 1 },
+        { noteNames: ['Gb3'], duration: 1 },
+        { noteNames: ['Cb4'], duration: 1 }
+      ],
+      key: "Cb"
+    },
+    {
+      sortedMelody: [ 'D3', 'D3', 'G3', 'A3' ],
+      melody: [
+        { noteNames: ['D3'], duration: 1 },
+        { noteNames: ['G3'], duration: 1 },
+        { noteNames: ['A3'], duration: 1 },
+        { noteNames: ['D3'], duration: 1 }
+      ],
+      key: "D"
+    },
+    {
+      sortedMelody: [ 'E#3', 'E#3', 'A#3', 'B#3' ],
+      melody: [
+        { noteNames: ['E#3'], duration: 1 },
+        { noteNames: ['A#3'], duration: 1 },
+        { noteNames: ['B#3'], duration: 1 },
+        { noteNames: ['E#3'], duration: 1 }
+      ],
+      key: "E#"
+    },
+  ];
+
+  test.each([0, 1, 2])("should generate quiz head text", (mathFloorReturnValue: number) => {
+    (<Mock>MathFloor).mockReturnValue(mathFloorReturnValue);
+    const quiz = new SingBassLines(SingBassLines.meta().getAllOptions);
+    expect(MathFloor).toBeCalledTimes(3);
+    expect(quiz.quizHead).toEqual([quizHeadOutput[mathFloorReturnValue]]);
+  });
+
+  test.each([0, 1, 2])("should generate solfege data to LogTable", async (mathFloorReturnValue: number) => {
+    (<Mock>MathFloor).mockReturnValue(mathFloorReturnValue);
+    (<Mock>LogTable.write).mockImplementation((solfege: typeof solfegeMelodies[number]) => {
+      expect(solfege.sortedMelody).toEqual(solfegeMelodies[mathFloorReturnValue].sortedMelody);
+      expect(solfege.melody).toEqual(solfegeMelodies[mathFloorReturnValue].melody);
+      expect(solfege.key).toEqual(solfegeMelodies[mathFloorReturnValue].key);
+    });
+    const quiz = new SingBassLines(SingBassLines.meta().getAllOptions);
+    await quiz.execute();
+    expect(LogTable.write).toBeCalledTimes(1);
+  });
+});
